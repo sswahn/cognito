93c40ce2a34a8b2c9c53c464412472caaa511b84
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-11-10 17:14:57 -0500

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import register from './core/Register/Register.js'
+import login from './core/Login/Login.js'
+import forgotPassword from './core/ForgotPassword/ForgotPassword.js'
+import confirmForgotPassword from './core/ConfirmForgotPassword/ConfirmForgotPassword.js'
+
+export {
+  register,
+  login,
+  forgotPassword,
+  confirmForgotPassword
+}
