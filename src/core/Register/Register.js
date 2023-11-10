840fbfdd3adc2ec091a8100711b9ec93b6445be4
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-11-10 16:43:04 -0500

    Create Register.js

---
+++ src/core/Register/Register.js
@@ -0,0 +1,16 @@
+import validator from './Validator.mjs'
+import model from './Model.js'
+
+const register = async event => {
+  try {
+    const body = JSON.parse(event.body)
+    const params = await validator(body)
+    const user = await model.createUser(params)
+    await model.addToGroup(params)
+    return  model.addToTable(user.UserSub, params) 
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+export default register
