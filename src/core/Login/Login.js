08587ae067b24d23231c4318d9d96d251ddae48a
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-11-10 16:58:54 -0500

    Update Login.js

--- src/core/Login/Login.js
+++ src/core/Login/Login.js
@@ -1,1 +1,14 @@
-//
+import validator from './Validator.js'
+import model from './Model.js'
+
+const login = async event => {
+  try {
+    const body = JSON.parse(event.body)
+    const params = await validator(body)
+    return model.authUser(params)
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+export default login
